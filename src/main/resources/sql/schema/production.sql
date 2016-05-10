f6a313439217f28ff0f28c8f482150780b06dbab
oknevor <rowenko1000@gmail.com>
2016-05-10 23:30:26 +0300

    Create production table scheme

---
+++ src/main/resources/sql/schema/production.sql
@@ -0,0 +1,12 @@
+CREATE TYPE CATEGORY AS ENUM ('clothes', 'makeup', 'scenery');
+
+CREATE TABLE IF NOT EXISTS agent (
+  id       INT         NOT NULL,
+  category CATEGORY    NOT NULL,
+  name     VARCHAR(64) NOT NULL,
+  price    MONEY       NOT NULL,
+  location VARCHAR(64),
+  available BOOL,
+  PRIMARY KEY (id)
+
+)
